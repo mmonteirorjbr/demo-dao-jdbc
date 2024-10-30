044f54d77fdfd64b293cd5bcfcab9d8aa2bf0ec5
mmonteirorjbr <marcelo.sant.anna.monteiro@gmail.com>
2024-10-30 16:18:20 -0300

    Classe da entidade Departamento

---
+++ src/applicacao/ProgramaAcessaDados.java
@@ -0,0 +1,13 @@
+package applicacao;
+
+import modelo.entidades.Departamento;
+
+public class ProgramaAcessaDados {
+
+	public static void main(String[] args) {
+		
+	   Departamento obj = new Departamento(1, "Livros");
+	   System.out.println(obj);
+	}
+
+}
